f703ef94ec59ac99a3bb7cc317271be4de52b627
1000ch <orz1000ch@gmail.com>
2013-10-30 02:42:34 +0900

    now working...

--- src/js/CookieManager.js
+++ src/js/CookieManager.js
@@ -1,321 +1,320 @@
 /**
  * CookieManager
- * Easy to delete history and cookie.
- *
+ * An easy way to manage your browser cookies.
  * Copyright 1000ch<http://1000ch.net/>
  */
 (function() {
 
-//cache
-var nativeForEach = [].forEach;
-var nativeSlice = [].slice;
+  //cache
+  var nativeForEach = [].forEach;
+  var nativeSlice = [].slice;
 
-/**
- * Get unique id.
- * @returns {Number}
- */
-var generateUniqueId = (function() {
-	var _cookieId = 0;
-	return function() {
-		++_cookieId;
-	};
-})();
+  /**
+   * Get unique id.
+   * @returns {Number}
+   */
+  var generateUniqueId = (function() {
+    var _cookieId = 0;
+    return function() {
+      ++_cookieId;
+    };
+  })();
 
-/**
- * Throttle 
- * @description borrowed from UnderscoreJS
- * @param {Function} func
- * @param {Number} wait
- * @returns {Function}
- */
-function throttle(fn, wait) {
-	var context, args, timeout, result;
-	var previous = 0;
-	var later = function() {
-		previous = new Date;
-		timeout = null;
-		result = fn.apply(context, args);
-	};
-	return function() {
-		var now = new Date;
-		var remaining = wait - (now - previous);
-		context = this;
-		args = arguments;
-		if (remaining <= 0) {
-			clearTimeout(timeout);
-			timeout = null;
-			previous = now;
-			result = fn.apply(context, args);
-		} else if (!timeout) {
-			timeout = setTimeout(later, remaining);
-		}
-		return result;
-	};
-}
+  /**
+   * Throttle
+   * @description borrowed from UnderscoreJS
+   * @param {Function} func
+   * @param {Number} wait
+   * @returns {Function}
+   */
+  function throttle(fn, wait) {
+    var context, args, timeout, result;
+    var previous = 0;
+    var later = function() {
+      previous = new Date;
+      timeout = null;
+      result = fn.apply(context, args);
+    };
+    return function() {
+      var now = new Date;
+      var remaining = wait - (now - previous);
+      context = this;
+      args = arguments;
+      if (remaining <= 0) {
+        clearTimeout(timeout);
+        timeout = null;
+        previous = now;
+        result = fn.apply(context, args);
+      } else if (!timeout) {
+        timeout = setTimeout(later, remaining);
+      }
+      return result;
+    };
+  }
 
-var CookieEntity = (function() {
-	/**
-	 * CookieEntity
-	 * @param {Cookie} cookie
-	 */
-	var _CookieEntity = function(cookie) {
-		this.cookieId = generateUniqueId();
-		this.name = cookie.name;
-		this.value = cookie.value;
-		this.domain = cookie.domain;
-		this.hostOnly = cookie.hostOnly;
-		this.path = cookie.path;
-		this.secure = cookie.secure;
-		this.httpOnly = cookie.httpOnly;
-		this.session = cookie.session;
-		this.expirationDate = cookie.expirationDate;
-		this.storeId = cookie.storeId;
-	};
-	/**
-	 * get completed url
-	 * @returns {String}
-	 */
-	_CookieEntity.prototype.url = function() {
-		return "http" + (this.secure ? "s" : "") + "://" + this.domain + this.path;
-	};
-	/**
-	 * get completed domain
-	 * @returns {String}
-	 */
-	_CookieEntity.prototype.completedDomain = function() {
-		var completedDomain = this.domain + "";
-		if(completedDomain.length && completedDomain.substring(0, 1) == ".") {
-			completedDomain = "*" + completedDomain;
-		}
-		return completedDomain;
-	};
-	return _CookieEntity;
-})();
+  var CookieEntity = (function() {
+    /**
+     * CookieEntity
+     * @param {Cookie} cookie
+     */
+    var _CookieEntity = function(cookie) {
+      this.cookieId = generateUniqueId();
+      this.name = cookie.name;
+      this.value = cookie.value;
+      this.domain = cookie.domain;
+      this.hostOnly = cookie.hostOnly;
+      this.path = cookie.path;
+      this.secure = cookie.secure;
+      this.httpOnly = cookie.httpOnly;
+      this.session = cookie.session;
+      this.expirationDate = cookie.expirationDate;
+      this.storeId = cookie.storeId;
+    };
+    /**
+     * get completed url
+     * @returns {String}
+     */
+    _CookieEntity.prototype.url = function() {
+      return "http" + (this.secure ? "s" : "") + "://" + this.domain + this.path;
+    };
+    /**
+     * get completed domain
+     * @returns {String}
+     */
+    _CookieEntity.prototype.completedDomain = function() {
+      var completedDomain = this.domain + "";
+      if(completedDomain.length && completedDomain.substring(0, 1) == ".") {
+        completedDomain = "*" + completedDomain;
+      }
+      return completedDomain;
+    };
+    return _CookieEntity;
+  })();
 
-var CookieCollection = (function() {
-	/**
-	 * CookieCollection
-	 * @param {Array<CookieEntity>} collection
-	 */
-	var _CookieCollection = function(collection) {
-		this.collection = nativeSlice.call(collection);
-	};
-	/**
-	 * Get entity which matches given domain.
-	 * @param {String} domain
-	 * @returns {Array}
-	 */
-	_CookieCollection.prototype.get = function(domain) {
-		var cookieEntities = [];
-		for(var i = 0, len = this.collection.length;i < len;i++) {
-			cookieEntity = this.collection[i];
-			if(cookieEntity.domain == domain) {
-				cookieEntities.push(cookieEntity);
-			}
-		}
-		return cookieEntities;
-	};
-	/**
-	 * Remove entity from collection.
-	 * @param {String} domain
-	 */
-	_CookieCollection.prototype.remove = function(cookieId) {
-		var removeIndex = -1;
-		var cookieEntity = null;
-		for(var i = 0, len = this.collection.length;i < len;i++) {
-			cookieEntity = this.collection[i];
-			if(cookieEntity.cookieId == cookieId) {
-				removeIndex = i;
-				break;
-			}
-		}
-		if(removeIndex != -1) {
-			this.collection.splice(removeIndex, 1);
-		}
-	};
-	_CookieCollection.prototype.each = function(callback) {
-		for(var i = 0, len = this.collection.length;i < len;i++) {
-			callback(this.collection[i], i, this.collection);
-		}
-	};
-	return _CookieCollection;
-})();
+  var CookieCollection = (function() {
+    /**
+     * CookieCollection
+     * @param {Array<CookieEntity>} collection
+     */
+    var _CookieCollection = function(collection) {
+      this.collection = nativeSlice.call(collection);
+    };
+    /**
+     * Get entity which matches given domain.
+     * @param {String} domain
+     * @returns {Array}
+     */
+    _CookieCollection.prototype.get = function(domain) {
+      var cookieEntities = [];
+      for(var i = 0, len = this.collection.length;i < len;i++) {
+        cookieEntity = this.collection[i];
+        if(cookieEntity.domain == domain) {
+          cookieEntities.push(cookieEntity);
+        }
+      }
+      return cookieEntities;
+    };
+    /**
+     * Remove entity from collection.
+     * @param {String} domain
+     */
+    _CookieCollection.prototype.remove = function(cookieId) {
+      var removeIndex = -1;
+      var cookieEntity = null;
+      for(var i = 0, len = this.collection.length;i < len;i++) {
+        cookieEntity = this.collection[i];
+        if(cookieEntity.cookieId == cookieId) {
+          removeIndex = i;
+          break;
+        }
+      }
+      if(removeIndex != -1) {
+        this.collection.splice(removeIndex, 1);
+      }
+    };
+    _CookieCollection.prototype.each = function(callback) {
+      for(var i = 0, len = this.collection.length;i < len;i++) {
+        callback(this.collection[i], i, this.collection);
+      }
+    };
+    return _CookieCollection;
+  })();
 
-var CookieView = (function() {
-	/**
-	 * CookieView
-	 * @{CookieCollection} cookieCollection
-	 */
-	var _CookieView = function(cookieCollection) {
-		this.cookieCollection = cookieCollection;
-	};
-	/**
-	 * Generate html string.
-	 * @returns {String}
-	 */
-	_CookieView.prototype.get = function() {
-		var shownDomain = [];
-		var html = "";
-		var domain = "";
-		this.cookieCollection.collection.forEach(function(cookieEntity) {
-			domain = cookieEntity.domain;
-			if(shownDomain.indexOf(domain) == -1) {
-				shownDomain.push(domain);
-				html += 
-					"<li data-domain='" + domain +  "'>" +
-						"<a href='" + cookieEntity.url() + "' target='_blank'>" + 
-							cookieEntity.completedDomain() + 
-						"</a>" +
-						"<button class='btn js-delete' data-domain='" + domain + "'>" + 
-							"<i class='icon-remove'></i> DELETE" + 
-						"</button>" + 
-					"</li>";
-			}
-			
-		});
-		return html;
-	};
-	return _CookieView;
-})();
+  var CookieView = (function() {
+    /**
+     * CookieView
+     * @{CookieCollection} cookieCollection
+     */
+    var _CookieView = function(cookieCollection) {
+      this.cookieCollection = cookieCollection;
+    };
+    /**
+     * Generate html string.
+     * @returns {String}
+     */
+    _CookieView.prototype.get = function() {
+      var shownDomain = [];
+      var html = "";
+      var domain = "";
+      this.cookieCollection.collection.forEach(function(cookieEntity) {
+        domain = cookieEntity.domain;
+        if(shownDomain.indexOf(domain) == -1) {
+          shownDomain.push(domain);
+          html +=
+            "<li data-domain='" + domain +  "'>" +
+              "<a href='" + cookieEntity.url() + "' target='_blank'>" +
+              cookieEntity.completedDomain() +
+              "</a>" +
+              "<button class='btn js-delete' data-domain='" + domain + "'>" +
+              "<i class='glyphicon glyphicon-remove'></i> DELETE" +
+              "</button>" +
+              "</li>";
+        }
 
-/**
- * CookieAccess
- */
-var CookieAccess = {
-	/**
-	 * Get cookie
-	 * @param {Object} details
-	 * @param {Function} callback
-	 */
-	get: function(details, callback) {
-		chrome.cookies.get(details, callback);
-	},
-	/**
-	 * Get cookies
-	 * @param {Object} details
-	 * @param {Function} callback
-	 */
-	getAll: function(details, callback) {
-		chrome.cookies.getAll(details, callback);
-	},
-	/**
-	 * Set cookie
-	 * @param {Object} details
-	 * @param {Function} callback
-	 */
-	set: function(details, callback) {
-		chrome.cookies.set(details, callback);
-	},
-	/**
-	 * Remove cookie
-	 * @param {CookieEntity} entity
-	 * @param {Function} callback
-	 */
-	remove: function(entity, callback) {
-		chrome.cookies.remove({
-			url: entity.url(),
-			name: entity.name,
-			storeId: entity.storeId
-		}, callback);
-	}
-};
+      });
+      return html;
+    };
+    return _CookieView;
+  })();
 
-var container = null;
-var search = null;
+  /**
+   * CookieAccess
+   */
+  var CookieAccess = {
+    /**
+     * Get cookie
+     * @param {Object} details
+     * @param {Function} callback
+     */
+    get: function(details, callback) {
+      chrome.cookies.get(details, callback);
+    },
+    /**
+     * Get cookies
+     * @param {Object} details
+     * @param {Function} callback
+     */
+    getAll: function(details, callback) {
+      chrome.cookies.getAll(details, callback);
+    },
+    /**
+     * Set cookie
+     * @param {Object} details
+     * @param {Function} callback
+     */
+    set: function(details, callback) {
+      chrome.cookies.set(details, callback);
+    },
+    /**
+     * Remove cookie
+     * @param {CookieEntity} entity
+     * @param {Function} callback
+     */
+    remove: function(entity, callback) {
+      chrome.cookies.remove({
+        url: entity.url(),
+        name: entity.name,
+        storeId: entity.storeId
+      }, callback);
+    }
+  };
 
-var cookieView = null;
-var cookieCollection = null;
+  var container = null;
+  var search = null;
 
-/**
- * delegate button hover event
- */
-function bindEventHandler() {
-	container.addClass("has-hover");
-}
+  var cookieView = null;
+  var cookieCollection = null;
 
-/**
- * undelegate button hover event
- */
-function unbindEventHandler() {
-	container.removeClass("has-hover");
-}
+  /**
+   * delegate button hover event
+   */
+  function bindEventHandler() {
+    container.addClass("has-hover");
+  }
 
-var scrollTimerId = null;
-window.addEventListener("scroll", function() {
-	if(!scrollTimerId) {
-		unbindEventHandler();
-	} else {
-		window.clearTimeout(scrollTimerId);
-	}
-	scrollTimerId = window.setTimeout(function() {
-		scrollTimerId = null;
-		bindEventHandler();
-	}, 300);
-})
+  /**
+   * undelegate button hover event
+   */
+  function unbindEventHandler() {
+    container.removeClass("has-hover");
+  }
 
-//when document is ready, call init
-$(document).ready(function() {
-	//cache elements
-	container = $("#js-cookies");
-	search = $("#js-search");
+  var scrollTimerId = null;
+  window.addEventListener("scroll", function() {
+    if(!scrollTimerId) {
+      unbindEventHandler();
+    } else {
+      window.clearTimeout(scrollTimerId);
+    }
+    scrollTimerId = window.setTimeout(function() {
+      scrollTimerId = null;
+      bindEventHandler();
+    }, 300);
+  })
 
-	//get all cookies
-	CookieAccess.getAll({}, function(cookies) {
-		var cookieArray = [];
-		nativeForEach.call(cookies, function(cookie) {
-			cookieArray.push(new CookieEntity(cookie));
-		});
-		cookieCollection = new CookieCollection(cookieArray);
-		cookieView = new CookieView(cookieCollection);
-		container.append(cookieView.get());
-	});
+  //when document is ready, call init
+  $(document).ready(function() {
+    //cache elements
+    container = $("#js-cookies");
+    search = $("#js-search");
+
+    //get all cookies
+    CookieAccess.getAll({}, function(cookies) {
+      var cookieArray = [];
+      nativeForEach.call(cookies, function(cookie) {
+        cookieArray.push(new CookieEntity(cookie));
+      });
+      cookieCollection = new CookieCollection(cookieArray);
+      cookieView = new CookieView(cookieCollection);
+      container.append(cookieView.get());
+    });
 
 
-	//bind event to container
-	container.on("click", ".js-delete", function() {
-		var clickedDomain = this.getAttribute("data-domain");
-		var relatedCookies = cookieCollection.get(clickedDomain);
+    //bind event to container
+    container.on("click", ".js-delete", function() {
+      var clickedDomain = this.getAttribute("data-domain");
+      var relatedCookies = cookieCollection.get(clickedDomain);
 
-		//remove row
-		$(this).parent("li").fadeOut(function() {
-			$(this).remove();
-		});
+      //remove row
+      $(this).parent("li").fadeOut(function() {
+        $(this).remove();
+      });
 
-		//remove cookies related with domain
-		relatedCookies.forEach(function(relatedCookie) {
-			CookieAccess.remove(relatedCookie, function(details) {
-				console.log(details);
-			});
-		});
-	});
+      //remove cookies related with domain
+      relatedCookies.forEach(function(relatedCookie) {
+        CookieAccess.remove(relatedCookie, function(details) {
+          console.log(details);
+        });
+      });
+    });
 
-	$("#js-delete-all").on("click", function() {
-		cookieCollection.each(function(cookieEntity, index, cookieCollection) {
-			CookieAccess.remove(cookieEntity, function(details) {
-				console.log(details);
-			});
-		});
-		cookieCollection = new CookieCollection([]);
-		cookieView = new CookieView(cookieCollection);
-		container.append(cookieView.get());
-	});
+    $("#js-delete-all").on("click", function() {
+      cookieCollection.each(function(cookieEntity, index, cookieCollection) {
+        CookieAccess.remove(cookieEntity, function(details) {
+          console.log(details);
+        });
+      });
+      cookieCollection = new CookieCollection([]);
+      cookieView = new CookieView(cookieCollection);
+      container.append(cookieView.get());
+    });
 
-	bindEventHandler();
+    bindEventHandler();
 
-	search.on("keyup", throttle(function() {
-		var word = $(this).val();
-		var row, domain;
-		var rows = container.find("li");
-		for(var i = 0, len = rows.length;i < len;i++) {
-			row = rows[i];
-			domain = row.getAttribute("data-domain") + "";
-			if(domain.indexOf(word) == -1) {
-				$(row).hide();
-			} else {
-				$(row).show();
-			}
-		}
-	}, 1000));
-});
+    search.on("keyup", throttle(function() {
+      var word = $(this).val();
+      var row, domain;
+      var rows = container.find("li");
+      for(var i = 0, len = rows.length;i < len;i++) {
+        row = rows[i];
+        domain = row.getAttribute("data-domain") + "";
+        if(domain.indexOf(word) == -1) {
+          $(row).hide();
+        } else {
+          $(row).show();
+        }
+      }
+    }, 1000));
+  });
 
 })();
